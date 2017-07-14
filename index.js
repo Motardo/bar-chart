64a12c01c2bbdf28dd88b0a5e5c04b7225497831
Nathan Wilson <nswilson@gmail.com>
2017-07-14 17:02:22 -0400

    Add bar geometry functions

--- index.js
+++ index.js
@@ -12,13 +12,32 @@ function getBarOffset(index, totalBars) {
   return (100 / totalBars) * index;
 }
 
-function getBarHeight(magnitude, index, totalBars) {
+function getBarHeight(magnitude, maximum) {
+  if (magnitude === 0) return 0;
+  return 100 * magnitude / maximum;
+}
 
+/* map an array of magnitudes into bar heights, widths and offsets */
+function getBars(data) {
+  const totalBars = data.length;
+  const width = 100 / totalBars;
+  let maximum = 0;
+
+  if (totalBars) maximum = Math.max(...data);
+
+  return data.map((magnitude, index) => {
+    return {
+      height: getBarHeight(magnitude, maximum),
+      width,
+      offset: getBarOffset(index, totalBars),
+    };
+  });
 }
 
 /* Create a bar chart SVG */
 module.exports = {
   getBarOffset,
   getBarHeight,
+  getBars,
   svg,
 };
--- test/test.js
+++ test/test.js
@@ -19,4 +19,56 @@ describe('#getBarOffset', function() {
     expect(getBarOffset(0, 3)).to.equal(0);
     expect(getBarOffset(0, 7)).to.equal(0);
   });
+
+  it('should return the correct offset', function() {
+    expect(getBarOffset(0, 1)).to.equal(0);
+    expect(getBarOffset(1, 2)).to.equal(50);
+    expect(getBarOffset(3, 5)).to.equal(60);
+    expect(getBarOffset(3, 9)).to.be.approximately(100/3, 0.0001);
+  });
+});
+
+describe('#getBarHeight', function() {
+  const getBarHeight = barChart.getBarHeight;
+
+  it('should return the correct height', function() {
+    expect(getBarHeight(0, 0)).to.equal(0);
+    expect(getBarHeight(0, 42)).to.equal(0);
+    expect(getBarHeight(3, 9)).to.be.approximately(100/3, 0.0001);
+  });
+});
+
+describe('#getBars', function() {
+  const getBars = barChart.getBars;
+
+  it('should handle empty data', function() {
+    expect(getBars([])).to.eql([]);
+  });
+
+  it('should handle a single data point', function() {
+    expect(getBars([0])).to.eql([{
+      height: 0,
+      width: 100,
+      offset: 0,
+    }]);
+    expect(getBars([1])).to.eql([{
+      height: 100,
+      width: 100,
+      offset: 0,
+    }]);
+    expect(getBars([5])).to.eql([{
+      height: 100,
+      width: 100,
+      offset: 0,
+    }]);
+  });
+
+  it('should handle multiple points', function() {
+    expect(getBars([0, 1, 0, 2])).to.eql([
+      {height: 0, width: 25, offset: 0},
+      {height: 50, width: 25, offset: 25},
+      {height: 0, width: 25, offset: 50},
+      {height: 100, width: 25, offset: 75},
+    ]);
+  });
 });
